9338c8cd2acf262f655097a7d84051407f92fa5d
Debbie Green <P110122341@student.peterborough.ac.uk>
2021-01-07 20:43:20 +0000

    Kept Forgetting to Push changes.

--- CatToy.cs
+++ CatToy.cs
@@ -4,9 +4,9 @@ namespace VirtualPet
 {
     class CatToy : Item, IToy
     {
-        public CatToy() : base("Laser Pointer", 7, "The Elusive Red Dot", 50, 10 )
+        public CatToy() : base( "Laser Pointer", 7, "Elusive Red Dot", 20, 15)
         {
-            
+           
         }
 
         public int ToyValue
